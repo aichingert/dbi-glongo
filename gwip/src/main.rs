09ea2efcee32072ba8bfbe5ca51cfe8be28d948b
tobias <aichinger.tobias13@gmail.com>
2022-07-30 13:03:23 +0200

    Created window

--- gwip/src/main.rs
+++ gwip/src/main.rs
@@ -1,3 +1,22 @@
+use eframe::{epi::App, run_native, egui::CentralPanel, NativeOptions};
+
+struct Gwip;
+
+impl App for Gwip {
+    fn update(&mut self, ctx: &eframe::egui::CtxRef, frame: &mut eframe::epi::Frame<'_>) {
+        CentralPanel::default().show(ctx, | ui | {
+            ui.label("Article");
+        });
+    }
+
+    fn name(&self) -> &str {
+        "Get Weather Information"
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let app: Gwip = Gwip;
+    let window_options: NativeOptions = NativeOptions::default();
+
+    run_native(Box::new(app), window_options);
 }
